0cf5910ba1f1a1ca58086cdb181a7a484757846a
arjav <arjav@192.168.2.134>
2022-05-12 09:35:33 -0400

    Test5 scroll by windows error

--- Arjav_TestNG/src/main/java/pages/Test5.java
+++ Arjav_TestNG/src/main/java/pages/Test5.java
@@ -3,18 +3,27 @@ package pages;
 import java.io.IOException;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import base.TestBase;
 
 public class Test5 extends TestBase{
+	
+	
+	WebElement buttonfive = driver.findElement(By.xpath("//button[@id='test5-button']"));
 
-	WebDriverWait wait = new WebDriverWait(driver, 10);
-	WebElement button5 = wait.until(
-	        ExpectedConditions.visibilityOfElementLocated(By.id("test5-button"))); 
+	Point p = buttonfive.getLocation();
+    int X = p.getX();
+    int Y = p.getY();
+    //scroll with Javascript Executor
+    JavascriptExecutor j = (JavascriptExecutor) driver;
+    j.executeScript("window.scrollBy(" + X + ", " + Y + ");");
+    
+		
 	
 	public Test5() throws IOException {
 		PageFactory.initElements(driver, this);
@@ -22,7 +31,7 @@ public class Test5 extends TestBase{
 	
 	public void button5() throws InterruptedException {
 		
-		button5.click();
+		buttonfive.click();
 	}
 	
 }
--- Arjav_TestNG/src/test/java/testcases/button5ClickAssert.java
+++ Arjav_TestNG/src/test/java/testcases/button5ClickAssert.java
@@ -1,5 +1,6 @@
 package testcases;
 
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -24,6 +25,11 @@ Test5 test5;
 	@Test
 	public void clickButton5() throws InterruptedException {
 		test5.button5();
+	}
+	
+	//@AfterMethod
+	public void tearDown() {
+		driver.quit();
 	}
 	
 }
